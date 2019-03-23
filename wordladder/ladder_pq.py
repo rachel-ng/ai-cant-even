28cf8e447b2e6e02022b7b82610d881771bde5fb
rachel-ng <rng@stuy.edu>
2019-03-22 21:54:39 -0400

    for testing for longest

--- wordladder/ladder_pq.py
+++ wordladder/ladder_pq.py
@@ -183,6 +183,7 @@ def try_all ():
             if len(re) > 10:
                 if len(re) > len(longest):
                     longest = re
+                    print(str(len(re)) + "\t" + str(re))
                 s += str(len(re)) + "\t" + str(re) + "\n"
 
     o_file = open("superlong.txt", "w+")
