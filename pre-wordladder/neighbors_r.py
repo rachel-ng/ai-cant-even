49747555de9916df34d080bbd9ea3faf963e21a5
rachel-ng <rng@stuy.edu>
2019-03-25 10:26:57 -0400

    optimization

--- pre-wordladder/neighbors_r.py
+++ pre-wordladder/neighbors_r.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import sys
-from datetime import datetime
 
 def neighbors (in_f, out_f):
     try:
@@ -23,12 +22,13 @@ def neighbors (in_f, out_f):
 
         dct = dict([])
         for word in d_file:
-            s = word[:i] + "_" + word[i+1:]
-            dct[word] = list(set.union(*[set(d[s]) for i in ln]) - set([word]))
+            for i in ln:
+                s = word[:i] + "_" + word[i+1:]
+                dct[word] =  dct.get(word, []) + d[s]
 
         s = ""
         for i in input_f:
-            s += i + "," + str(len(dct[i])) + "\n"
+            s += i + "," + str(len(set(dct[i]) - set([i]))) + "\n"
 
         o_file = open(out_f, "w+")
         o_file.write(s)
@@ -38,9 +38,7 @@ def neighbors (in_f, out_f):
         print("so uh, " + in_f + " don't exist broski")
 
 if __name__ == "__main__":
-    start = datetime.now()
     neighbors(sys.argv[1], sys.argv[2])
-    print((datetime.now()-start).microseconds / 1000000)
 
 
-    #python3 neighbors_d.py  harry.txt  answers.txt
+    #python3 neighbors_r.py  harry.txt  answers.txt
