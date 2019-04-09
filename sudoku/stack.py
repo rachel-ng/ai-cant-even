d745004bf133c486b8521af217dd3e1a37a17460
rachel-ng <rng@stuy.edu>
2019-04-09 11:25:55 -0400

    changed test cases

--- sudoku/stack.py
+++ sudoku/stack.py
@@ -1,37 +1,38 @@
 class Stack:
     def __init__ (self, size=100):
         self.size = 0 # self.size - 1 == index of item at position
-        self.stack = [None] * size
+        self.list = [None] * size
 
     def push (self, data):
-        if self.size - 1 > len(self.stack): self.stack + [data]
-        else: self.stack[self.size] = data
+        if self.size - 1 > len(self.list): self.stack + [data]
+        else: self.list[self.size] = data
         self.size += 1
 
     def pop (self):
-        popped, self.stack[self.size - 1] = self.stack[self.size - 1], None
-        self.size -= 1
-        return popped
+        if self.size < 1:
+            return None
+        else:
+            popped, self.list[self.size - 1] = self.list[self.size - 1], None
+            self.size -= 1
+            return popped
 
     def peek(self):
-        return self.stack[self.size - 1]
+        return self.list[self.size - 1]
+
+    def stack(self):
+        return self.list[:self.size]
 
 st = Stack(10)
+
+print(st.stack())
 print(st.peek())
 print(st.pop())
-st.push(1)
-st.push(2)
-st.push(9)
-print(st.peek())
-print(st.pop())
-st.push(5)
-st.push(7)
-print(st.peek())
-print(st.pop())
-st.push(3)
-st.push(5)
-st.push(9)
-print(st.stack)
-print(st.peek())
-print(st.pop())
-print(st.stack)
+
+te = [1,2,9,5,7,16,17,5,9,10]
+for i in te:
+    if i % 3 == 0:
+        print(st.peek())
+        print(st.pop())
+    else:
+        st.push(i)
+        print(st.stack())
