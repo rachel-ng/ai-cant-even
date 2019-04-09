f46fd0f2dccf6e78fab8fa15b01679deb3e23526
rachel-ng <rng@stuy.edu>
2019-04-09 11:25:46 -0400

    s.py

--- sudoku/s.py
+++ sudoku/s.py
@@ -6,8 +6,11 @@ def s_board (in_f):
     input_f = [line.rstrip().split(",") for line in i_file]
     i_file.close()
     board = [[int(n) if n != '_' else 0 for n in i] for i in input_f]
+
+    print("board")
     for i in board:
         print (i)
+
     rec = dict([[i,[[0] * 9,[0] * 9]] for i in range(10)[1:]])
     #rec_2 = dict([[i,[set([]),set([])]] for i in range(10)[1:]])
     possible = dict([[i,[[],[]]] for i in range(10)[1:]])
@@ -35,11 +38,13 @@ def s_board (in_f):
             c += 1
         r += 1
 
+    print("squares")
     for i in squares:
         print(i)
         for x in squares[i]:
             print(x)
         print("")
+    print("board")
     for i in board:
         print (i)
     for i in rec:
@@ -50,16 +55,16 @@ def s_board (in_f):
         possible[i][0] =  [n for n,p in enumerate(rec[i][0],0) if p == 0]#r
         possible[i][1] =  [n for n,p in enumerate(rec[i][1],0) if p == 0]#c
 
-    print(rec)
+    print("rec",rec)
     #print(rec_2)
-    print(possible)
+    print("possible",possible)
     #print(possible == rec_2)
-    print(squares)
-    for i in range(9):
-        print(i)
-        print(i % 3)
-        print(i // 3 + 1)
-        print('\n')
+    print("squares",squares)
+    # for i in range(9):
+    #     print(i)
+    #     print(i % 3)
+    #     print(i // 3 + 1)
+    #     print('\n')
 
 if __name__ == "__main__":
      s_board(sys.argv[1])
