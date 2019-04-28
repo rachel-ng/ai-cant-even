94ecfa627d8821313974a5bba8296f0a66b05437
rachel-ng <rng@stuy.edu>
2019-04-28 03:00:48 -0400

    neighbors stuff

--- 8_sudoku-algorithms/sudoku-smart.py
+++ 8_sudoku-algorithms/sudoku-smart.py
@@ -24,8 +24,6 @@ notes: made them all slower, some (S O M E) had fewer backtracks, other didn't
 
 
 
-
-
 '''
 
 class Stack: # FILO
@@ -72,6 +70,13 @@ cliques=[[0,1,2,3,4,5,6,7,8],\
          [57,58,59,66,67,68,75,76,77],\
          [60,61,62,69,70,71,78,79,80]]
 
+cliques_r = [[0,1,2,3,4,5,6,7,8],[9,10,11,12,13,14,15,16,17],[18,19,20,21,22,23,24,25,26],[27,28,29,30,31,32,33,34,35],[36,37,38,39,40,41,42,43,44],[45,46,47,48,49,50,51,52,53],[54,55,56,57,58,59,60,61,62],[63,64,65,66,67,68,69,70,71],[72,73,74,75,76,77,78,79,80]]
+
+cliques_c = [[0,9,18,27,36,45,54,63,72],[1,10,19,28,37,46,55,64,73],[2,11,20,29,38,47,56,65,74],[3,12,21,30,39,48,57,66,75],[4,13,22,31,40,49,58,67,76],[5,14,23,32,41,50,59,68,77],[6,15,24,33,42,51,60,69,78],[7,16,25,34,43,52,61,70,79],[8,17,26,35,44,53,62,71,80]]
+
+cliques_s = [[0,1,2,9,10,11,18,19,20],[3,4,5,12,13,14,21,22,23],[6,7,8,15,16,17,24,25,26],[27,28,29,36,37,38,45,46,47],[30,31,32,39,40,41,48,49,50],[33,34,35,42,43,44,51,52,53],[54,55,56,63,64,65,72,73,74],[57,58,59,66,67,68,75,76,77],[60,61,62,69,70,71,78,79,80]]
+
+
 def getBoard(argv):
     input_file = open(INPUT_FILE,'r+').read().strip().split('\n')
     board = []
@@ -91,6 +96,14 @@ def makeNeighbors():
     global d
     d = dict([[i,{member for clique in cliques for member in clique if i in clique and i != member}] for i in range(81)])
     return d
+
+def makeNeighbors2():
+    global r, c, s
+    r = dict([[i,{member for clique in cliques_r for member in clique if i in clique and i != member}] for i in range(81)]) # n // 9
+    c = dict([[i,{member for clique in cliques_c for member in clique if i in clique and i != member}] for i in range(81)]) # n % 9
+    s = dict([[i,{member for clique in cliques_s for member in clique if i in clique and i != member}] for i in range(81)]) # (rs // 3) * 3 + (cs // 3)
+    return r, c, s
+
 
 def nextOpenCell(board, start):
     try: return board.index(0, start+1)
@@ -134,7 +147,6 @@ NEW_CELL = 0
 FIND_NEXT_CELL = 1
 BACKTRACK = 2
 AllVals = set([1,2,3,4,5,6,7,8,9])
-places = set(range(81))
 
 INPUT_FILE = sys.argv[1]
 OUTPUT_FILE = sys.argv[2]
@@ -147,9 +159,15 @@ def main(argv=None):
         argv = sys.argv
 
     name,board = getBoard(BOARD_TO_SOLVE)
-
+    possible_places = set(range(81))
+    places = possible_places & set([n for n,p in enumerate(board) if p != 0 ])
+    print(places)
     mystack = Stack()
     makeNeighbors()
+    makeNeighbors2()
+    print(r)
+    print(c)
+    print(s)
     nback = 0
     ntrials = 0
     cell = nextOpenCell(board,-1)
